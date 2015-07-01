d34a02a04a9199c795f16b8851e7f3ba3b9ba8ca
Munir Wanis <munirwanis@users.noreply.github.com>
2015-07-01 11:52:16 -0300

    examples added

---
+++ CarFunctioning/InterfaceExample/WithInterface/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithInterface
+{
+    public interface ICarro
+    {
+        void Partida();
+    }
+
+    public class Uno : ICarro
+    {
+        public void Partida()
+        {
+            Console.WriteLine("Uno deu a partida");
+        }
+    }
+    
+    public class Ferrari : ICarro
+    {
+        public void Partida()
+        {
+            Console.WriteLine("Ferrari deu partida");
+        }
+    }
+    
+    public class Gol : ICarro
+    {
+        public void Partida()
+        {
+            Console.WriteLine("Gol deu a partida");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Uno uno = new Uno();
+
+            Gol gol = new Gol();
+
+            Program.Action(uno);
+
+            Program.Action(gol);
+
+            Program.Action(new Ferrari());
+
+            Console.Read();
+        }
+
+        public static void Action(ICarro carro)
+        {
+            carro.Partida();
+        }
+    }
+}
---
+++ CarFunctioning/InterfaceExample/WithoutInterface/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutInterface
+{
+    public class Uno
+    {
+        public void Partida()
+        {
+            Console.WriteLine("Uno deu partida");
+        }
+    }
+
+    public class Gol
+    {
+        public void Partida()
+        {
+            Console.WriteLine("Gol deu partida");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Uno uno = new Uno();
+
+            Gol gol = new Gol();
+
+            Program.Action(uno);
+
+            Program.Action(gol);
+
+            Console.Read();
+        }
+
+        public static void Action(Uno uno)
+        {
+            uno.Partida();
+        }
+
+        public static void Action(Gol gol)
+        {
+            gol.Partida();
+        }
+    }
+}
